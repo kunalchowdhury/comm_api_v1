f10102bdf4dd1e8ccdb5824c93fc406350f2b208
Kunal Chowdhury <kunalkumar.chowdhury@gmail.com>
2016-05-06 15:23:21 +0530

    dummy

--- avr_xbee_comm/avr_xbee_comm/main_slave.c
+++ avr_xbee_comm/avr_xbee_comm/main_slave.c
@@ -8,11 +8,16 @@
 #include "header/serial_sender_receiver.h"
 #include "header/util.h"
 
+volatile uint16_t address_16 =0;
+
 int main1(void)
 {
    while(1)
    {
-	 // uint8_t * work_request = rx_serial();
+	 uint8_t *work_request = rx_serial();
+	 if(address_16)
+         address_16 = get_self_xbee_16_id(); 	 
+	 if(is_valid_zb_req(work_request, address) )
    }
 	
 }
